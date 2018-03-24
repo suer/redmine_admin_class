b921ba0883fbb0ae325d67b96221fc7610f0c09c
Ryo SUETSUGU <suetsugu.r@gmail.com>
2018-03-24 18:15:07 +0900

    Rails5 support

--- lib/admin_class_application_helper_patch.rb
+++ lib/admin_class_application_helper_patch.rb
@@ -1,17 +1,8 @@
 require_dependency 'application_helper'
 module AdminClass
-  module ApplicationHelperPatch
-    def self.included(base)
-      base.send(:include, ApplicationHelperMethods)
-      base.class_eval do
-        alias_method_chain :body_css_classes, :admin_class
-      end
-    end
-  end
-
-  module ApplicationHelperMethods
-    def body_css_classes_with_admin_class
-      body_css_classes_without_admin_class + (User.current.admin? ? ' admin' : '')
+  module ApplicationHelperWithAdminClass
+    def body_css_classes
+      super + (User.current.admin? ? ' admin' : '')
     end
   end
 end
